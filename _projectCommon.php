0518665e093bda481647e0dc0f242ffdc65c72cf
spearce <spearce>
2010-02-02 23:11:25 +0000

    Correct menu links with / suffixes, they are all directories

--- _projectCommon.php
+++ _projectCommon.php
@@ -11,22 +11,22 @@
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
 	$Nav->setLinkList(array());
-	$Nav->addNavSeparator("JGit", 	"/jgit");
-	$Nav->addCustomNav("Download", "/jgit/download", "_self", 3);
-	$Nav->addCustomNav("Documentation", "/jgit/documentation", "_blank", 3);
-	$Nav->addCustomNav("Support", "/jgit/support", "_blank", 3);
-	$Nav->addCustomNav("Getting Involved", "/jgit/developers", "_blank", 3);
+	$Nav->addNavSeparator("JGit", 	"/jgit/");
+	$Nav->addCustomNav("Download", "/jgit/download/", "_self", 3);
+	$Nav->addCustomNav("Documentation", "/jgit/documentation/", "_blank", 3);
+	$Nav->addCustomNav("Support", "/jgit/support/", "_blank", 3);
+	$Nav->addCustomNav("Getting Involved", "/jgit/developers/", "_blank", 3);
 	
 	$pageKeywords	= "eclipse, egit, jgit, git, vcs";
 	$pageAuthor		= "Chris Aniszczyk";
 	$pageTitle 		= "JGit";
 
 	$Menu->setMenuItemList(array());
-	$Menu->addMenuItem("Home", "/jgit", "_self");
-	$Menu->addMenuItem("Download", "/jgit/download", "_self");
-	$Menu->addMenuItem("Documentation", "/jgit/documentation", "_self");
-	$Menu->addMenuItem("Support", "/jgit/support", "_self");
-	$Menu->addMenuItem("Developers", "/jgit/developers", "_self");
+	$Menu->addMenuItem("Home", "/jgit/", "_self");
+	$Menu->addMenuItem("Download", "/jgit/download/", "_self");
+	$Menu->addMenuItem("Documentation", "/jgit/documentation/", "_self");
+	$Menu->addMenuItem("Support", "/jgit/support/", "_self");
+	$Menu->addMenuItem("Developers", "/jgit/developers/", "_self");
 	
 	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style.css"/>' . "\n\t");
 	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style2.css"/>' . "\n\t");
