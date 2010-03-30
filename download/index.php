27d436cac56b239c3d5e8b3d18030c320bbe2881
spearce <spearce>
2010-03-30 01:12:50 +0000

    Parameterize the version number in our downloads so its easier to update on new releases.

--- download/index.php
+++ download/index.php
@@ -1,10 +1,13 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	$pageTitle 		= "JGit - Download";
+	$version      = "0.7.1";
 
 	$html  = '<div id="midcolumn">';
 	$html .= file_get_contents('_index.html');
 	$html .= "</div>";
-	
+
+	$html = preg_replace('/@VERSION@/', $version, $html);
+
 	# Generate the web page
 	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
