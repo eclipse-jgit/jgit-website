e71d77b810de5cf28ad47ea963a9b9b0251816a7
msohn <msohn>
2011-12-23 14:00:04 +0000

    Update version for release 1.2

--- download/index.php
+++ download/index.php
@@ -1,6 +1,6 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	$pageTitle 		= "JGit - Download";
-	$version      = "1.1.0.201109151100-r";
+	$version      = "v1.2.0.201112221803-r";
 
 	$html  = '<div id="midcolumn">';
 	$html .= file_get_contents('_index.html');
