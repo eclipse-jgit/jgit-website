d48fc4a7c9f50ae54b27fe2c570dea3e2e307f35
msohn <msohn>
2012-02-17 10:33:18 +0000

    Fix JGit version number

--- download/index.php
+++ download/index.php
@@ -1,6 +1,6 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	$pageTitle 		= "JGit - Download";
-	$version      = "v1.3.0.201202151440-r";
+	$version      = "1.3.0.201202151440-r";
 
 	$html  = '<div id="midcolumn">';
 	$html .= file_get_contents('_index.html');
