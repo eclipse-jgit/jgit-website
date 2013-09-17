b58960cbb9bf4e9d42e9a149dc9a47dc813a0f2f
Matthias Sohn <matthias.sohn@sap.com>
2013-09-17 13:17:52 +0200

    Update download page for JGit 3.0.3
    Change-Id: Ia00da8b12ef82f308bfe18bc0108b10d634e9f79
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- download/index.php
+++ download/index.php
@@ -1,12 +1,14 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	$pageTitle 		= "JGit - Download";
-	$version      = "3.0.0.201306101825-r";
+	$maven_repo		= "https://repo.eclipse.org/content/groups/releases/";
+	$version      = "3.0.3.201309161630-r";
 
 	$html  = '<div id="midcolumn">';
 	$html .= file_get_contents('_index.html');
 	$html .= "</div>";
 
 	$html = preg_replace('/@VERSION@/', $version, $html);
+	$html = preg_replace('/@MAVEN_REPO@/', $maven_repo, $html);
 
 	# Generate the web page
 	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
