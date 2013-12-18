c72425e875a6ae9b4723e579c808b4f5a984d1b5
Matthias Sohn <matthias.sohn@sap.com>
2013-12-18 22:14:07 +0100

    Update download page for 3.2.0
    Change-Id: Ie663bd6274a58d3d05106204ed6ee96000efe469
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- download/index.php
+++ download/index.php
@@ -1,7 +1,7 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	$pageTitle 		= "JGit - Download";
 	$maven_repo		= "https://repo.eclipse.org/content/groups/releases/";
-	$version      = "3.1.0.201310021548-r";
+	$version      = "3.2.0.201312181205-r";
 
 	$html  = '<div id="midcolumn">';
 	$html .= file_get_contents('_index.html');
