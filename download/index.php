87e8713deab75c746e4c6a1ff5c4a53fddd34928
Matthias Sohn <matthias.sohn@sap.com>
2014-09-26 15:36:46 +0200

    Update JGit website for 3.5.0
    Change-Id: If064e5b9eeca5f6918bbaa4bef06d1dcb9be6d56
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- download/index.php
+++ download/index.php
@@ -1,7 +1,7 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	$pageTitle 		= "JGit - Download";
 	$maven_repo		= "https://repo.eclipse.org/content/groups/releases/";
-	$version      = "3.4.1.201406201815-r";
+	$version      = "3.5.0.201409260305-r";
 
 	$html  = '<div id="midcolumn">';
 	$html .= file_get_contents('_index.html');
