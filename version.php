df73d1ad3d2b65d4373380275327d84581ffbc8a
Matthias Sohn <matthias.sohn@sap.com>
2018-07-10 01:05:36 +0200

    Update api_version to 5.0.1
    Maven site for 5.0.1 was uploaded to the download server.
    
    Change-Id: I91c937e242b80e38728b62d408d94beb6b9f37bc
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
 	$version = "5.0.1.201806211838-r";
-	$api_version="5.0.0.201806131550-r"; // For API docs and CLIRR reports
+	$api_version="5.0.1.201806211838-r"; // For API docs and CLIRR reports
 ?>
