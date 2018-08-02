dcabd4e9f601936a5fa69f31b563a84099f7402c
Matthias Sohn <matthias.sohn@sap.com>
2018-08-02 17:01:54 +0200

    Update download page for 5.0.2
    Change-Id: Id50545ffee92164eda974f81dcf2ab92d44f867b

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
-	$version = "5.0.1.201806211838-r";
-	$api_version="5.0.1.201806211838-r"; // For API docs and CLIRR reports
+	$version = "5.0.2.201807311906-r";
+	$api_version="5.0.2.201807311906-r"; // For API docs and CLIRR reports
 ?>
