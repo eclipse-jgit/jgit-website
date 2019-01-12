4b16dbf85982b94d4479a16f4af7d1825b6fc842
Matthias Sohn <matthias.sohn@sap.com>
2019-01-12 01:29:56 +0100

    Update download page for 5.2.1.201812262042-r
    Change-Id: I307abed9e83241bdab4ed87c11ca712b9f98a920

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
-	$version = "5.2.0.201812061821-r";
-	$api_version="5.2.0.201812061821-r"; // For API docs and CLIRR reports
+	$version = "5.2.1.201812262042-r";
+	$api_version="5.2.1.201812262042-r"; // For API docs and CLIRR reports
 ?>
