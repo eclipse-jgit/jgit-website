dd11b0d8721a01d6c6ba667c756abbfc21551d46
Matthias Sohn <matthias.sohn@sap.com>
2020-03-18 16:46:02 +0100

    Update homepage for 5.7.0.202003110725-r
    Change-Id: I0e3b4fa4bdd88dd15df470330fdc2abd3384d954
    Signed-off-by: Matthias Sohn <matthias.sohn@sap.com>

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
-	$version = "5.6.1.202002131546-r";
-	$api_version="5.6.1.202002131546-r"; // For API docs and CLIRR reports
+	$version = "5.7.0.202003110725-r";
+	$api_version="5.7.0.202003110725-r"; // For API docs and CLIRR reports
 ?>
