a2c8b308eed28900f14ee5d07245f2eec1da45c6
Matthias Sohn <matthias.sohn@sap.com>
2021-06-14 12:25:58 +0200

    Update homepage for v5.12.0.202106070339-r
    Change-Id: Ibba6417e617f7b731dc381995b68a4818b7a852a

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
-	$version = "5.11.1.202105131744-r";
-	$api_version="5.11.1.202105131744-r"; // For API docs and CLIRR reports
+	$version = "5.12.0.202106070339-r";
+	$api_version="5.12.0.202106070339-r"; // For API docs and CLIRR reports
 ?>
