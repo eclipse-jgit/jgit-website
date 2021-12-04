4dc62d6fb565003ccf276cb4f311493bfe627e14
Matthias Sohn <matthias.sohn@sap.com>
2021-12-04 01:32:52 +0100

    Update homepage for 6.0.0.202111291000-r
    Change-Id: I5d561cf241eb683e9f1c05638d5a5777383caa83

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
-	$version = "5.13.0.202109080827-r";
-	$api_version="5.13.0.202109080827-r"; // For API docs and CLIRR reports
+	$version = "6.0.0.202111291000-r";
+	$api_version="6.0.0.202111291000-r"; // For API docs and CLIRR reports
 ?>
