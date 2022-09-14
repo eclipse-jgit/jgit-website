409be20401bcfaff3a1e2174b5c8833f1d8e0c6e
Matthias Sohn <matthias.sohn@sap.com>
2022-09-14 13:40:42 +0200

    Update for 6.3.0.202209071007-r
    Change-Id: Id12825f3e453aacc0501136e9e76174bb081e50a

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
-	$version = "6.2.0.202206071550-r";
-	$api_version="6.2.0.202206071550-r"; // For API docs and CLIRR reports
+	$version = "6.3.0.202209071007-r";
+	$api_version="6.3.0.202209071007-r"; // For API docs and CLIRR reports
 ?>
