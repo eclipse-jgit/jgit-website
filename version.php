f57aa5b9cf66a73fffdf300d1e1fd241ee5df595
Matthias Sohn <matthias.sohn@sap.com>
2023-06-13 10:43:49 +0200

    Update for 6.6.0.202305301015-r
    Change-Id: I320e7f41817198ebb584353016933ee749244116

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
-	$version = "6.5.0.202303070854-r";
-	$api_version="6.5.0.202303070854-r"; // For API docs and CLIRR reports
+	$version = "6.6.0.202305301015-r";
+	$api_version="6.6.0.202305301015-r"; // For API docs and CLIRR reports
 ?>
